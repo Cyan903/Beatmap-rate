558cd84b2863fce6161f6c3e2ceaaf7ecbb19a7e
CyanPiano <cyanp0515@gmail.com>
2022-03-02 19:43:30 -0500

    Update HitObjects to adjust to BPM

--- src/functions.ts
+++ src/functions.ts
@@ -1,20 +1,21 @@
+import consola from "consola";
 import { getTimingPoints, getHitObjects, loadFile } from "./parser/parser";
 
 // prettier-ignore
 const filePath = "";
 
-export function updateTimingPoints(timingPoints: any[], RATE: number) {
+export function updateTimingPoints(timingPoints: any[], rate: number) {
     return timingPoints.map((p) => {
         const item = {
             point: p[0].split(","),
             bpm: p[1],
         };
 
-        // PARSE FLOAT NOT PARSE INT I GOT IT YESSSSSSSSSSSSSSSSSSSSSSSSSSS
-        item.point[0] = String(parseFloat(item.point[0]) / RATE);
+        item.point[0] = String(parseFloat(item.point[0]) / rate);
+        consola.info(`changing bpm | ${item.bpm} -> ${item.bpm * rate}`);
 
         if (parseFloat(item.point[1]) > 0) {
-            item.point[1] = String(parseFloat(item.point[1]) / RATE);
+            item.point[1] = String(parseFloat(item.point[1]) / rate);
         }
 
         return item.point.join(",");
@@ -22,15 +23,17 @@ export function updateTimingPoints(timingPoints: any[], RATE: number) {
 }
 
 // WILL NOT RUN UNLESS TIMING POINTS ARE DONE
-export function updateHitObjects() {}
+export function updateHitObjects(objects: any[], rate: number) {
+    return objects.map((n) => {
+        const note = n.split(",");
+        note[2] = String(parseFloat(note[2]) / rate);
+
+        return note.join(",");
+    });
+}
 
 const points = getTimingPoints(loadFile(filePath));
 const objects = getHitObjects(loadFile(filePath));
-const fixedPoints = updateTimingPoints(points, 1.3);
 
-console.log(fixedPoints.join("\n"));
-
-// console.log(parseObjects("HitObjects", loadFile(filePath)));
-// console.log(
-//     getHitObjects(loadFile(filePath))
-// )
+const fixedPoints = updateTimingPoints(points, 1.4);
+const fixedObjects = updateHitObjects(objects, 1.4);
