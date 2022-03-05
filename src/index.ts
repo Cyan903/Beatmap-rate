6787e3dc0ebf7c25ca375d9256e1c1c48bbf5630
CyanPiano <cyanp0515@gmail.com>
2022-03-05 07:44:52 -0500

    Faster method of making new file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { updateTimingPoints, updateHitObjects } from "./parser/functions";
-import { copyFile, readFile, writeFile } from "fs";
+import { readFileSync, writeFile } from "fs";
 import { loadFile, replaceAll, replaceValue } from "./parser/parser";
 import { modAudio, createFileName } from "./audio/ffmpeg";
 import consola from "consola";
@@ -25,47 +25,36 @@ export async function modBeatmap(
 
     const newHitObjects = updateHitObjects(map.hitObjects, rate);
     const newTimingPoints = updateTimingPoints(map.timingPoints, rate);
+    let data = await readFileSync(npath + filename, "utf-8");
 
-    copyFile(npath + filename, npath + newName, (err) => {
+    if (!data) {
+        consola.fatal(`[modBeatmap] error reading file ${newName}`);
+        process.exit(1);
+    }
+
+    data = replaceAll(data, map.hitObjects, newHitObjects);
+    data = replaceAll(data, map.timingPoints, newTimingPoints);
+    data = replaceValue(data, "AudioFilename", newAudio);
+    data = replaceValue(data, "Version", newVersion);
+    data = replaceValue(
+        data,
+        "PreviewTime",
+        String(~~(map.PreviewTime / rate))
+    );
+
+    writeFile(npath + newName, data, "utf8", (err) => {
         if (err) {
-            consola.fatal("[modBeatmap] could not create file");
-            process.exit(1);
-        }
-
-        consola.success(`created new file -> ${newName}`);
-    });
-
-    readFile(npath + newName, "utf8", (err, data) => {
-        if (err) {
-            consola.fatal(`[readFile] error reading file ${newName}`);
+            consola.fatal(`[readFile] error writting file ${newName}`);
             consola.log(err);
             process.exit(1);
         }
 
-        data = replaceAll(data, map.hitObjects, newHitObjects);
-        data = replaceAll(data, map.timingPoints, newTimingPoints);
-        data = replaceValue(data, "AudioFilename", newAudio);
-        data = replaceValue(data, "Version", newVersion);
-        data = replaceValue(
-            data,
-            "PreviewTime",
-            String(~~(map.PreviewTime / rate))
-        );
-
-        writeFile(npath + newName, data, "utf8", (err) => {
-            if (err) {
-                consola.fatal(`[readFile] error writting file ${newName}`);
-                consola.log(err);
-                process.exit(1);
-            }
-
-            consola.success(`modded new file -> ${newName}`);
-        });
+        consola.success(`modded new file -> ${newName}`);
     });
 }
 
 modBeatmap(
-    "Songs/1217647 Shiraishi - Moon-gate/",
-    "Shiraishi - Moon-gate (DrLou) [Hard].osu",
+    "./tmp/_bemodding/",
+    "Camellia - Kono Hoshi de.... (Raytoly) [Reality].osu",
     1.3
 );
